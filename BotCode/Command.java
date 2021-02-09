1018d5cbd1450f2cce9a0f0d78f23397d940fc57
kaneisgreat <76920883+kaneisgreat@users.noreply.github.com>
2021-02-09 15:38:58 -0500

    Update and rename Command.java to BotCode/Command.java

--- BotCode/Command.java
+++ BotCode/Command.java
@@ -3,6 +3,8 @@ package me.kaneisgreat;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import reactor.core.publisher.Mono;
 
+//an interface for Commands, which will be a Mono stream to implement a reactive system
 public interface Command {
+	//execute command based on corresponding event
 	Mono<Void> execute(MessageCreateEvent event);
 }
