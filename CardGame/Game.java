098948f56ade26367eba254e02731f5f17e1f9b4
kaneisgreat <76920883+kaneisgreat@users.noreply.github.com>
2021-02-09 15:41:04 -0500

    Update and rename Game.java to CardGame/Game.java

--- CardGame/Game.java
+++ CardGame/Game.java
@@ -1,6 +1,6 @@
 package cardgame;
 
-//abstract class for card games
+//abstract class for card games to branch off into other games besides War, such as poker
 public abstract class Game {
 	abstract void start();
 	abstract void deal();
